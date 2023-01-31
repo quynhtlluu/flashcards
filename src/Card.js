a7c19c43afb5740c2d1e21b99239895026d961bf
Quynh Luu <quynhtlluu@gmail.com>
2023-01-30 17:23:39 -0800

    Add properties to store answers and correct answers to Card class

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,8 @@ class Card {
     constructor(cardId, cardQuestions, possibleCardAnswers, correctCardAnswer) {
         this.id = cardId;
         this.question = cardQuestions;
+        this.answers = possibleCardAnswers;
+        this.correctAnswer = correctCardAnswer;
     }
 }
 
